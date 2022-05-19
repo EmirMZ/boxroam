06d16be86ae34049aa769be766d8f8f24f871895
[Emir Muhamad Zaid] <[emir.muhamad@ui.ac.id]>
2022-05-19 21:14:47 +0700

    attempt2

---
+++ server/login.js
@@ -0,0 +1,88 @@
+
+const { Client } = require('pg')
+const bp = require('body-parser')
+const express = require('express');
+const session = require('express-session');
+const path = require('path');
+const app = express();
+app.use(bp.json())
+app.use(bp.urlencoded({ extended: true }))
+
+
+const db = new Client({
+    //Tambahkan parameter yang sesuai untuk melakukan koneksi dengan database
+    user: 'postgres',
+    host: 'localhost',
+    database: 'emir_7',
+    password: '',
+    port: 5432
+})
+
+db.connect((err) => {
+    if (err) {
+        console.log(err)
+        return
+    }
+    console.log('Database berhasil terkoneksi')
+})
+
+
+
+app.use(session({
+	secret: 'secret',
+	resave: true,
+	saveUninitialized: true
+}));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, 'static')));
+
+// http://localhost:3000/
+app.get('/', function(request, response) {
+	// Render login template
+	response.sendFile(path.join(__dirname + '\public\login.html'));
+});
+
+// http://localhost:3000/auth
+app.post('/auth', function(request, response) {
+	// Capture the input fields
+	let username = request.body.username;
+	let password = request.body.password;
+	// Ensure the input fields exists and are not empty
+	if (username && password) {
+		// Execute SQL query that'll select the account from the database based on the specified username and password
+		connection.query('SELECT * FROM accounts WHERE username = ? AND password = ?', [username, password], function(error, results, fields) {
+			// If there is an issue with the query, output the error
+			if (error) throw error;
+			// If the account exists
+			if (results.length > 0) {
+				// Authenticate the user
+				request.session.loggedin = true;
+				request.session.username = username;
+				// Redirect to home page
+				response.redirect('/home');
+			} else {
+				response.send('Incorrect Username and/or Password!');
+			}			
+			response.end();
+		});
+	} else {
+		response.send('Please enter Username and Password!');
+		response.end();
+	}
+});
+
+// http://localhost:3000/home
+app.get('/home', function(request, response) {
+	// If the user is loggedin
+	if (request.session.loggedin) {
+		// Output username
+		response.send('Welcome back, ' + request.session.username + '!');
+	} else {
+		// Not logged in
+		response.send('Please login to view this page!');
+	}
+	response.end();
+});
+
+app.listen(3000);
