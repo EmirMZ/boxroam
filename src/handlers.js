405e1788185c2b37db6956f1920bf7dd27ecc338
[Emir Muhamad Zaid] <[emir.muhamad@ui.ac.id]>
2022-05-22 02:08:23 +0700

    perbaikan gedee

--- src/handlers.js
+++
@@ -1,8 +0,0 @@
-const model = require("./model");
-
-function home(request, response) {
-  response.writeHead(200, { "content-type": "text/html" });
-  response.end(`<h1>Hello world</h1>`);
-}
-
-module.exports = { home };
--- src/model.js
+++
@@ -1,5 +0,0 @@
-const db = require("./database/connection");
-
-// functions that select/insert data to the DB go here
-
-module.exports = {};
