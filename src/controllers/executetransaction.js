94b1276a4fe35b600adc143addc2f3ae31cbea5c
[Emir Muhamad Zaid] <[emir.muhamad@ui.ac.id]>
2022-05-29 23:37:43 +0700

    test

--- src/controllers/executetransaction.js
+++ src/controllers/executetransaction.js
@@ -1,10 +1,19 @@
 const axios = require('axios');
+const paydia = require('../constants');
+const db = require('../models/transactionModel');
 
-exports.getQR = async() => {
+exports.getQR = async(request) => {
+
+    // var trans = await db.addTrans(request)
     axios
-    .post('https://whatever.com/todos', {
-        todo: 'Buy the milk',
-    })
+    .post('https://api.paydia.id/qris/generate/', {
+        "merchantid": paydia.PAYDIA_MERCHANT,
+        "nominal": request.price,
+        "tip": 0,
+        "ref": request.bank_transaction_number,
+        "callback": "https://boxroam.azurewebsites.net/api/callback",
+        "expire": 5
+      })
     .then(res => {
         console.log(`statusCode: ${res.status}`);
         console.log(res);
@@ -12,5 +21,10 @@ exports.getQR = async() => {
     .catch(error => {
         console.error(error);
     });
+    
+}
 
+exports.gotCallback = async (req,res) =>{
+    // var trans = await db.addTrans(request)
+    console.log(req)
 }
